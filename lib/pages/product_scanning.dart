5efe21f0663c5f0a147d0a3bf3f119b1019a6d4a
UKM Seni Theatrisic <113005391+iannstronaut@users.noreply.github.com>
2024-12-13 17:44:42 +0700

    fix

--- lib/pages/product_scanning.dart
+++ lib/pages/product_scanning.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconsax_plus/iconsax_plus.dart';
 import 'package:raffaelosanzio/api/product_api.dart';
+import 'package:raffaelosanzio/api/profile_api.dart';
 import 'package:raffaelosanzio/models/hive/model.dart';
 import 'package:raffaelosanzio/shared/theme.dart';
 import 'package:raffaelosanzio/widget/product_item.dart';
 
 class ProductScanningPage extends StatefulWidget {
-  const ProductScanningPage({super.key});
+  final int skin_type;
+  const ProductScanningPage({super.key, required this.skin_type});
 
   @override
   State<ProductScanningPage> createState() => _ProductScanningPageState();
@@ -15,6 +17,7 @@ class ProductScanningPage extends StatefulWidget {
 
 class _ProductScanningPageState extends State<ProductScanningPage> {
   List<Product> _products = [];
+  User? _user;
   bool _isLoading = true;
   @override
   void initState() {
@@ -22,13 +25,17 @@ class _ProductScanningPageState extends State<ProductScanningPage> {
     super.initState();
     _fetchProductsFromHive();
   }
+
   Future<void> _fetchProductsFromHive() async {
     List<Product> products = await ProductApiHandler().getProducts();
+    var user = await UserApiHandler().getUser();
     setState(() {
+      _user = user;
       _products = products;
     });
     _isLoading = false;
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,7 +53,7 @@ class _ProductScanningPageState extends State<ProductScanningPage> {
           cursor: SystemMouseCursors.click,
           child: GestureDetector(
             onTap: () {
-              Navigator.pop(context);
+              Navigator.pushReplacementNamed(context, '/main-home');
             },
             child: SizedBox(
               width: 20,
@@ -60,12 +67,19 @@ class _ProductScanningPageState extends State<ProductScanningPage> {
           ),
         ),
       ),
-      body: _buildPopularProductsSection(context),
+      body: _buildPopularProductsSection(),
     );
   }
 
-  Widget _buildPopularProductsSection(BuildContext context) {
-    List<Map<String, dynamic>> formattedProducts = [];
+  Widget _buildPopularProductsSection() {
+    List<Product> data = _products
+        .where((element) =>
+            element.skin_type
+                .any((skinType) => skinType.skin_type == widget.skin_type) &&
+            User.jsonToGender(element.gender) == _user!.gender)
+        .toList();
+    List<Map<String, dynamic>> formattedProducts =
+        data.map((product) => product.toJson()).toList();
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -75,12 +89,15 @@ class _ProductScanningPageState extends State<ProductScanningPage> {
           physics: const NeverScrollableScrollPhysics(),
           childAspectRatio: MediaQuery.of(context).size.width /
               (MediaQuery.of(context).size.height * 0.68), // Sesuaikan proporsi
-          children: List.generate(formattedProducts.length, (index) {
-            final product = formattedProducts[index];
+          children: List.generate(
+            formattedProducts.length,
+            (index) {
+              final product = formattedProducts[index];
 
-            // Mengirimkan satu Map<String, dynamic> ke ProductItem
-            return ProductItem(product: product);
-          }),
+              // Mengirimkan satu Map<String, dynamic> ke ProductItem
+              return ProductItem(product: product);
+            },
+          ),
         )
       ],
     );
